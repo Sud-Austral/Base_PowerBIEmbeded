b3bc178a60e2eff40bb80c40461be2d9cdb70e2c
lmonsalve <lmonsalve22@gmail.com>
2020-12-24 19:20:11 -0300

    problema resuelto 3

--- V2/App Owns Data/PowerBIEmbedded_AppOwnsData/Controllers/DashboardController.cs
+++ V2/App Owns Data/PowerBIEmbedded_AppOwnsData/Controllers/DashboardController.cs
@@ -57,5 +57,63 @@ namespace PowerBIEmbedded_AppOwnsData.Controllers
                 return View(m_embedService.EmbedConfig);
             }
         }
+
+        public async Task<ActionResult> PruebaMapaNacional()
+        {
+            //ViewBag.codcom = id;
+            //ViewBag.columna = "Departamento";
+            //ViewBag.tabla = "Localiza_HN";
+            var embedResult = await m_embedService.EmbedReport(null, null, "90461ecc-1682-492c-af3f-41b2063619b9", "f9eafdc6-98a1-4aa9-a525-66b9a2136e0d");
+            if (embedResult)
+            {
+                return View(m_embedService.EmbedConfig);
+            }
+            else
+            {
+                return View(m_embedService.EmbedConfig);
+            }
+        }
+
+        public async Task<ActionResult> Pruebalenguistica(int id = 1)
+        { 
+            ViewBag.codcom = id;
+            ViewBag.columna = "Id_Dep";
+            ViewBag.tabla = "LOCALIZACION GT";
+            if(id == 0)
+            {
+                ViewBag.codcom = null;
+                ViewBag.columna = null;
+                ViewBag.tabla = null;
+            }
+            var embedResult = await m_embedService.EmbedReport(null, null, "fc8662a7-e884-4f56-a270-4c4ecb8d8136", "f9eafdc6-98a1-4aa9-a525-66b9a2136e0d");
+            if (id == 0)
+            {
+                embedResult = await m_embedService.EmbedReport(null, null, "d0402a92-a413-4cb5-9c28-10bd031b2a81", "f9eafdc6-98a1-4aa9-a525-66b9a2136e0d");
+            }
+            if (embedResult)
+            {
+                return View(m_embedService.EmbedConfig);
+            }
+            else
+            {
+                return View(m_embedService.EmbedConfig);
+            }
+        }
+
+        public async Task<ActionResult> PruebaHonduras(string id = "Colón")
+        {
+            ViewBag.codcom = id;
+            ViewBag.columna = "Departamento";
+            ViewBag.tabla = "Localiza_HN";
+            var embedResult = await m_embedService.EmbedReport(null, null, "cab9a957-99a6-4e49-826b-fe3506947703", "f4eee1998-74cb-4ddb-885b-3db5a4db255d");
+            if (embedResult)
+            {
+                return View(m_embedService.EmbedConfig);
+            }
+            else
+            {
+                return View(m_embedService.EmbedConfig);
+            }
+        }
     }
 }
